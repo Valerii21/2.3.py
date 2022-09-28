314a6709cf0d53a3e1edfec0e8b07f253128d6e2
Valerii21 <109736919+Valerii21@users.noreply.github.com>
2022-09-28 13:32:34 +0300

    Add files via upload

---
+++ 2.3.py
@@ -0,0 +1,13 @@
+"""16. Задать список из n чисел последовательности (1+1/n)^n 
+и вывести на экран их сумму """
+
+from msilib import sequence
+
+n = int(input('Введите число: ')) 
+
+def get_squerence(n):
+    return [round((1 + 1 / x)**x, 5) for x in range (1, n + 1)]
+
+nums = get_squerence(n)
+print(nums)
+print(round(sum(nums), 5))
